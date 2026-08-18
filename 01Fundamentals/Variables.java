8a03cad78c11917ef3895b0c2cfc5446cb497916
hiralirs <hiralirs@gmail.com>
2026-08-18 06:38:37 +0530

    Add Java variables lesson with comments

--- 01Fundamentals/Variables.java
+++ 01Fundamentals/Variables.java
@@ -2,10 +2,10 @@
 
 public class Variables {
     public static void main( String[] args){
-    String studentName = "Hirali";
-    int currentSemester= 3;
-    double currentGpa= 8.73;
-    boolean isEnrolled= true;
+    String studentName = "Hirali";        // String (Text)
+    int currentSemester= 3;               //Integer (Whole number)
+    double currentGpa= 8.73;              // Double (Java's version of float/decimal)
+    boolean isEnrolled= true;             // Boolean (True/False)
 
     System.out.println("Name: "+ studentName );
     System.out.println("GPA: "+ currentGpa);
