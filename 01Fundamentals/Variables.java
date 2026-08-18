124eb7e1b4fc4f10983d1bcc3413e6b59569ba7d
hiralirs <hiralirs@gmail.com>
2026-08-18 06:33:14 +0530

    Add Java variables fundamental lesson

---
+++ 01Fundamentals/Variables.java
@@ -0,0 +1,14 @@
+
+
+public class Variables {
+    public static void main( String[] args){
+    String studentName = "Hirali";
+    int currentSemester= 3;
+    double currentGpa= 8.73;
+    boolean isEnrolled= true;
+
+    System.out.println("Name: "+ studentName );
+    System.out.println("GPA: "+ currentGpa);
+
+    }
+}
